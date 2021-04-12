9b5e6ae810851444540b50859398a7cd1ad90177
caroline223 <67647836+caroline223@users.noreply.github.com>
2021-04-11 20:25:54 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your code here
+
+const formData = {
+    name: 'Steve', 
+    email: 'steve@steve.com'
+}
+
+const configurationObject = {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+    },
+    body: JSON.stringify(formData)
+};
+
+function submitData() {
+    return fetch('http://localhost:3000/users', configurationObject)
+    .then(function(response){
+        return response.json();
+    })
+     .then(function(object) {
+      document.body.innerHTML = object['id'];
+    })
+    .catch(function(error) {
+      alert("Unauthorized Access");
+      document.body.innerHTML = error.message
+    })
+}
